a8f2ac133167d8e0f96551fe575c9b0219613065
MEHEDY-HASSAN <mehedyhassan865@gmail.com>
2022-05-17 22:36:31 +0600

    Added in new branch

---
+++ src/main/AA.java
@@ -0,0 +1,18 @@
+package main;
+
+public class AA {
+    int M;
+    int K;
+
+    public AA(int k) {
+        K = k;
+    }
+
+    public int getK() {
+        return K;
+    }
+
+    public void setK(int k) {
+        K = k;
+    }
+}
