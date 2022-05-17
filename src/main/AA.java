3b86d5a263d12f218adee46b4e6d8258be638347
MEHEDY-HASSAN <mehedyhassan865@gmail.com>
2022-05-17 22:28:19 +0600

    new java class

---
+++ src/main/AA.java
@@ -0,0 +1,17 @@
+package main;
+
+public class AA {
+    string s;
+
+    public AA(string s) {
+        this.s = s;
+    }
+
+    public string getS() {
+        return s;
+    }
+
+    public void setS(string s) {
+        this.s = s;
+    }
+}
