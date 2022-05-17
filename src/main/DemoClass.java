b68ddef9e6e8800a47f003cb07231cdca7e11b1e
MEHEDY-HASSAN <mehedyhassan865@gmail.com>
2022-05-17 22:16:44 +0600

    DEMO CLASS ADDED

---
+++ src/main/DemoClass.java
@@ -0,0 +1,17 @@
+package main;
+
+public class DemoClass {
+    int x;
+
+    public DemoClass(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
